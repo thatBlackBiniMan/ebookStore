6ab27e3915644bff770e1f15cff1d7b5eae1eeb0
@thatBlackBiniMan <solomonomoruyi66@gmail.com>
2022-12-26 16:35:22 +0100

    Finished Homepage

--- ebook.js
+++ ebook.js
@@ -1,9 +1,17 @@
 searchForm = document.querySelector('.search-form');
 
-document.querySelector('#search-btn').onclick =()=>{
-  searchForm.classList.toggle('active');
+document.querySelector('#search-btn').onclick = () =>{
+searchForm.classList.toggle('active');
 }
 
+let loginForm  = document.querySelector('.login-form-container');
+  document.querySelector('#login-btn').onclick = ()=> {
+  loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = ()=> {
+  loginForm.classList.remove('active');
+}
 
 window.onscroll = () =>{
   searchForm.classList.remove('active');
@@ -22,3 +30,25 @@ window.onload = () =>{
     document.querySelector('.header .header-2').classList.remove('active');
    }
 }
+
+
+
+var swiper = new Swiper(".books-slider", {
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
