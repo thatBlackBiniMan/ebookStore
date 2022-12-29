249cef047efd4677f1dd0aea021d88a17136d808
@thatBlackBiniMan <solomonomoruyi66@gmail.com>
2022-12-29 19:18:10 +0100

    Complete

--- ebook.js
+++ ebook.js
@@ -29,9 +29,19 @@ window.onload = () =>{
    } else {
     document.querySelector('.header .header-2').classList.remove('active');
    }
+
+   fadeOut();
 }
 
 
+function loader(){
+  document.querySelector('.loader-container').classList.add('active');
+}
+
+
+function fadeOut(){
+  setTimeout(loader, 3000);
+}
 
 var swiper = new Swiper(".books-slider", {
   loop:true,
